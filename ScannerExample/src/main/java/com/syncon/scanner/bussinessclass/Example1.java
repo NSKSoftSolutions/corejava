290a6ee964c18822ab700c8c1417480e24b170ea
Sajjad Alikhan <navabsajjad.ali@gmail.com>
2018-09-02 15:54:59 +0530

    Example1 ---> getEmployee()

---
+++ ScannerExample/src/main/java/com/syncon/scanner/bussinessclass/Example1.java
@@ -0,0 +1,12 @@
+package com.syncon.scanner.bussinessclass;
+
+public class Example1 {
+
+	public void details_of_employee() {
+
+		String emp_name = "XYZ";
+		System.out.println("Employee Name:" + emp_name);
+
+	}
+
+}
